1c39ea1d15a87fe1ad7c5febc5a44f90e8aeb911
JpunktWpunkt <wittig.jessica@googlemail.com>
2022-11-16 08:53:51 +0100

    add the huddle design and components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import {ThemeProvider} from "styled-components";
+import GlobalStyles from "../styles/Gobal";
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+  mobile: '375px',
+  desktop: '1440px',
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <ThemeProvider theme={theme}>
+    <>
+      <GlobalStyles />
+  <Component {...pageProps} />
+    </>
+    </ThemeProvider>
+)
 }
 
 export default MyApp
---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import SocialIcons from "./SocialIcons";
+import {Container} from './styles/Container.styled';
+import {Flex} from './styles/Flex.styled';
+import {StyledFooter, Logo} from "./styles/Footer.styled";
+
+const Footer = () => {
+    return (
+        <StyledFooter>
+<Container>
+    <Logo src='./images/logo_bw.svg' alt='' fill='#fff'/>
+    <Flex>
+        <ul>
+            <li>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua
+            </li>
+            <li>+1-543-123-4567</li>
+            <li>example@huddle.com</li>
+        </ul>
+        <ul>
+            <li>About Us</li>
+            <li>What We Do</li>
+            <li>FAQ</li>
+        </ul>
+
+        <ul>
+            <li>Career</li>
+            <li>Blog</li>
+            <li>Contact Us</li>
+        </ul>
+        <SocialIcons />
+    </Flex>
+    <p>&copy; 2022 JpunktWpunkt. All rights reserved</p>
+</Container>
+        </StyledFooter>
+    );
+}
+
+
+export default Footer;
